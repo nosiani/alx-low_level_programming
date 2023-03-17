f25ceb6ac569f91312a129e879b38b3a48b48952
nosiani <nosiani.ceo@gmail.com>
2023-03-16 20:57:35 -0700

    penultimate commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point into the program
+ * This program prints a statement using puts
+ * Return: always zero
+ */
+
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+
+
+}
