9a63e53f9f1170004acc63388b505976e7ef56a3
nosiani <nosiani.ceo@gmail.com>
2023-03-23 20:58:22 -0700

    More functions

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers -This program prints numbers 0-9
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
